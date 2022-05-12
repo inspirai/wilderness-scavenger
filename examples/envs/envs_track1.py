34bab5733bd1b33a13efccd59e60043140c2b28e
zqp <626717190@qq.com>
2022-05-12 21:39:24 +0800

    add env render to visualize depth_map

--- examples/envs/envs_track1.py
+++ examples/envs/envs_track1.py
@@ -4,6 +4,8 @@ from gym import spaces
 from ray.rllib.env import EnvContext
 from inspirai_fps.utils import get_distance, get_position
 from inspirai_fps.gamecore import Game, ActionVariable
+import os
+import cv2
 
 BASE_WORKER_PORT = 50000
 
@@ -12,6 +14,7 @@ class BaseEnv(gym.Env):
     def __init__(self, config: EnvContext):
         super().__init__()
 
+        self.config = config
         self.record = config.get("record", False)
         self.replay_suffix = config.get("replay_suffix", "")
         self.print_log = config.get("detailed_log", False)
@@ -26,15 +29,18 @@ class BaseEnv(gym.Env):
         self.game.set_episode_timeout(config["timeout"])
         self.game.set_random_seed(config["random_seed"])
         self.start_location = config.get("start_location", [0, 0, 0])
-
-    def reset(self):
-        print("Reset for a new game ...")
-        self._reset_game_config()
         if self.record:
             self.game.turn_on_record()
         else:
             self.game.turn_off_record()
         self.game.set_game_replay_suffix(self.replay_suffix)
+        if self.config["use_depth"]:
+            self.game.turn_on_depth_map()
+
+    def reset(self):
+        print("Reset for a new game ...")
+        self._reset_game_config()
+
         self.game.new_episode()
         self.state = self.game.get_state()
         self.running_steps = 0
@@ -45,7 +51,26 @@ class BaseEnv(gym.Env):
         return super().close()
 
     def render(self, mode="replay"):
-        return None
+        if not self.config["use_depth"]:
+            raise Exception("You do not turn on the use-depth-map parameter,"
+                            "so there is no chance of visualizing depth-map for you")
+        
+        SCALE = 1
+        WIDTH,HEIGHT,FAR = self.game.get_depth_map_size()
+            
+        def visualize(map_id, ts, depth_map):
+            img = (depth_map / FAR * 255).astype(np.uint8)
+            h, w = img.shape
+            img = cv2.resize(img, (w * SCALE, h * SCALE))
+            img = cv2.applyColorMap(img, cv2.COLORMAP_JET)
+            cv2.imwrite(f"dmp/depth_map={map_id}_ts={ts}.png", img)
+            
+        os.makedirs("dmp", exist_ok=True)
+        depth_map = self.state.depth_map
+        visualize(self.config["map_id"], self.game.get_time_step(), depth_map)
+
+
+
 
     def _reset_game_config(self):
         raise NotImplementedError()
@@ -66,6 +91,16 @@ class NavigationBaseEnv(BaseEnv):
         self.game.set_game_mode(Game.MODE_NAVIGATION)
         self.game.set_target_location(self.target_location)
 
+        self.action_pools = {
+            ActionVariable.WALK_DIR: [0, 90, 180, 270],
+            ActionVariable.WALK_SPEED: [3, 6],
+        }
+        self.action_space = spaces.MultiDiscrete([len(pool) for pool in self.action_pools.values()])
+        self.observation_space = spaces.Box(low=-1, high=1, shape=(3,), dtype=np.float32)
+
+        self.game.set_available_actions([action_name for action_name in self.action_pools.keys()])
+        self.game.init()
+
     def _reset_game_config(self):
         self.start_location = self._sample_start_location()
         self.game.set_start_location(self.start_location)
@@ -77,14 +112,13 @@ class NavigationBaseEnv(BaseEnv):
         done = self.game.is_episode_finished()
         reward = 0
         self.running_steps += 1
+        cur_pos = get_position(self.state)
+        tar_pos = self.target_location
 
+        if get_distance(cur_pos, tar_pos) <= self.trigger_range:
+            reward += 100
+            done = True
         if done:
-            cur_pos = get_position(self.state)
-            tar_pos = self.target_location
-
-            if get_distance(cur_pos, tar_pos) <= self.trigger_range:
-                reward += 100
-            
             if self.print_log:
                 Start = np.round(np.asarray(self.start_location), 2).tolist()
                 Target = np.round(np.asarray(self.target_location), 2).tolist()
@@ -95,25 +129,6 @@ class NavigationBaseEnv(BaseEnv):
 
         return self._get_obs(), reward, done, {}
 
-    def _sample_start_location(self):
-        raise NotImplementedError()
-
-    def _action_process(self, action):
-        raise NotImplementedError()
-
-
-class NavigationEnvSimple(NavigationBaseEnv):
-    def __init__(self, config: EnvContext):
-        super().__init__(config)
-        self.action_pools = {
-            ActionVariable.WALK_DIR: [0, 90, 180, 270],
-            ActionVariable.WALK_SPEED: [3, 6],
-        }
-        self.action_space = spaces.MultiDiscrete([len(pool) for pool in self.action_pools.values()])
-        self.observation_space = spaces.Box(low=-1, high=1, shape=(3,), dtype=np.float32)
-
-        self.game.set_available_actions([action_name for action_name in self.action_pools.keys()])
-        self.game.init()
 
     def _get_obs(self):
         cur_pos = np.asarray(get_position(self.state))
@@ -134,3 +149,5 @@ class NavigationEnvSimple(NavigationBaseEnv):
         x = self.target_location[0] + dx
         z = self.target_location[2] + dz
         return [x, self.start_hight, z]
+
+
