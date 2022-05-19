98396657c60fcfbc596b727c2ef9a0f2358026ee
zqp <626717190@qq.com>
2022-05-19 14:09:52 +0800

    redesign state ,reward,start_loc

--- examples/baseline_track1_navigation.py
+++ examples/baseline_track1_navigation.py
@@ -1,4 +1,5 @@
 import argparse
+import sys
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-T", "--timeout", type=int, default=60 * 2)  # The time length of one game (sec)
@@ -9,49 +10,101 @@ parser.add_argument("--target-location", type=float, nargs=3, default=[0, 0, 0])
 parser.add_argument("--start-location", type=float, nargs=3, default=[0, 0, 0])
 parser.add_argument("--start-range", type=float, default=2)
 parser.add_argument("--start-hight", type=float, default=5)
-parser.add_argument("--engine-dir", type=str, default="../unity3d")
-parser.add_argument("--map-dir", type=str, default="../data")
-parser.add_argument("--num-workers", type=int, default=0)
+parser.add_argument("--engine-dir", type=str, default="../wildscav-linux-backend")
+parser.add_argument("--map-dir", type=str, default="../map_data")
+parser.add_argument("--num-workers", type=int, default=4)
 parser.add_argument("--eval-interval", type=int, default=None)
 parser.add_argument("--record", action="store_true")
 parser.add_argument("--replay-suffix", type=str, default="")
 parser.add_argument("--checkpoint-dir", type=str, default="checkpoints_track1")
 parser.add_argument("--detailed-log", action="store_true", help="whether to print detailed logs")
-parser.add_argument("--run", type=str, default="PPO", help="The RLlib-registered algorithm to use.")
-parser.add_argument("--stop-iters", type=int, default=9999)
-parser.add_argument("--stop-timesteps", type=int, default=100000000)
+parser.add_argument("--run", type=str, default="ppo", help="The RLlib-registered algorithm to use.")
+parser.add_argument("--stop-iters", type=int, default=300)
+parser.add_argument("--stop-timesteps", type=int, default=1e8)
 parser.add_argument("--stop-reward", type=float, default=95)
-
+parser.add_argument("--use-depth", action="store_true")
+parser.add_argument("--stop-episodes", type=float, default=10000)
 
 if __name__ == "__main__":
     import os
     import ray
     from ray.tune.logger import pretty_print
     from ray.rllib.agents.ppo import PPOTrainer
-    from envs.envs_track1 import NavigationEnvSimple
+    from ray.rllib.agents.a3c import A3CTrainer
+    # from ray.rllib.agents.sac import SACTrainer
+    from ray.rllib.agents.impala import ImpalaTrainer
+    # from ray.rllib.agents.ddpg import DDPGTrainer
+    # from ray.rllib.agents.dqn.apex import ApexTrainer
+    from ray.rllib.agents.ppo.appo import APPOTrainer
+    from ray.rllib.agents.ppo.ddppo import DDPPOTrainer
+    from envs.envs_track1 import NavigationBaseEnv
 
     args = parser.parse_args()
 
     ray.init()
-    trainer = PPOTrainer(
+    alg = args.run
+    if alg =='ppo':
+        trainer = PPOTrainer(
         config={
-            "env": NavigationEnvSimple,
+            "env": NavigationBaseEnv,
             "env_config": vars(args),
             "framework": "torch",
             "num_workers": args.num_workers,
             "evaluation_interval": args.eval_interval,
         }
     )
-
+    elif alg=='appo':
+        trainer = APPOTrainer(
+        config={
+            "env": NavigationBaseEnv,
+            "env_config": vars(args),
+            "framework": "torch",
+            "num_workers": args.num_workers,
+            "evaluation_interval": args.eval_interval,
+            "num_gpus":0
+        }
+    )
+    elif alg=='a3c':
+        trainer = A3CTrainer(
+        config={
+            "env": NavigationBaseEnv,
+            "env_config": vars(args),
+            "framework": "torch",
+            "num_workers": args.num_workers,
+            "evaluation_interval": args.eval_interval,
+            "num_gpus":0
+        }
+    )
+    elif alg=='impala':
+        trainer = ImpalaTrainer(
+        config={
+            "env": NavigationBaseEnv,
+            "env_config": vars(args),
+            "framework": "torch",
+            "num_workers": args.num_workers,
+            "evaluation_interval": args.eval_interval,
+            "num_gpus":0
+        }
+    )
+    else:
+        raise ValueError('No such algorithm')
+    step=0
     while True:
+        step+=1
         result = trainer.train()
         print(pretty_print(result))
+        if step !=0 and step %30==0:
+            os.makedirs(args.checkpoint_dir+f"{alg}", exist_ok=True)
+            trainer.save_checkpoint(args.checkpoint_dir)
 
-        if result["episode_reward_mean"] >= args.stop_reward:
-            os.makedirs(args.checkpoint_dir, exist_ok=True)
+        if result["episodes_total"] >= args.stop_episodes:
+            os.makedirs(args.checkpoint_dir+f"{alg}", exist_ok=True)
             trainer.save_checkpoint(args.checkpoint_dir)
             trainer.stop()
             break
 
-    print(pretty_print(result))
+    print("the training has done!!")
     ray.shutdown()
+    sys.exit()
+
+
--- examples/envs/envs_track1.py
+++ examples/envs/envs_track1.py
@@ -1,9 +1,13 @@
+import random
+
 import gym
 import numpy as np
 from gym import spaces
 from ray.rllib.env import EnvContext
 from inspirai_fps.utils import get_distance, get_position
 from inspirai_fps.gamecore import Game, ActionVariable
+import os
+import cv2
 
 BASE_WORKER_PORT = 50000
 
@@ -12,6 +16,7 @@ class BaseEnv(gym.Env):
     def __init__(self, config: EnvContext):
         super().__init__()
 
+        self.config = config
         self.record = config.get("record", False)
         self.replay_suffix = config.get("replay_suffix", "")
         self.print_log = config.get("detailed_log", False)
@@ -26,18 +31,23 @@ class BaseEnv(gym.Env):
         self.game.set_episode_timeout(config["timeout"])
         self.game.set_random_seed(config["random_seed"])
         self.start_location = config.get("start_location", [0, 0, 0])
-
-    def reset(self):
-        print("Reset for a new game ...")
-        self._reset_game_config()
         if self.record:
             self.game.turn_on_record()
         else:
             self.game.turn_off_record()
+           
         self.game.set_game_replay_suffix(self.replay_suffix)
+        self.episodes = 0
+
+
+    def reset(self):
+        print("Reset for a new game ...")
+        self._reset_game_config()
+
         self.game.new_episode()
         self.state = self.game.get_state()
         self.running_steps = 0
+        self.episodes+=1
         return self._get_obs()
 
     def close(self):
@@ -45,7 +55,26 @@ class BaseEnv(gym.Env):
         return super().close()
 
     def render(self, mode="replay"):
-        return None
+        if not self.config.get("use_depth",None):
+            raise Exception("You do not turn on the use-depth-map parameter,"
+                            "so there is no chance of visualizing depth-map for you")
+        
+        SCALE = 1
+        WIDTH,HEIGHT,FAR = self.game.get_depth_map_size()
+            
+        def visualize(map_id, ts, depth_map):
+            img = (depth_map / FAR * 255).astype(np.uint8)
+            h, w = img.shape
+            img = cv2.resize(img, (w * SCALE, h * SCALE))
+            img = cv2.applyColorMap(img, cv2.COLORMAP_JET)
+            cv2.imwrite(f"dmp/depth_map={map_id}_ts={ts}.png", img)
+            
+        os.makedirs("dmp", exist_ok=True)
+        depth_map = self.state.depth_map
+        visualize(self.config["map_id"], self.game.get_time_step(), depth_map)
+
+
+
 
     def _reset_game_config(self):
         raise NotImplementedError()
@@ -60,11 +89,46 @@ class NavigationBaseEnv(BaseEnv):
 
         self.start_range = config["start_range"]
         self.start_hight = config["start_hight"]
-        self.trigger_range = self.game.get_target_reach_distance()
         self.target_location = config["target_location"]
 
         self.game.set_game_mode(Game.MODE_NAVIGATION)
         self.game.set_target_location(self.target_location)
+        if config["use_depth"]:
+            self.game.turn_on_depth_map()
+
+        self.action_pools = {
+            ActionVariable.WALK_DIR: [0, 45, 90, 135, 180, 225, 270],
+            ActionVariable.WALK_SPEED: [1, 3, 6],
+        }
+
+        
+        self.action_space = spaces.MultiDiscrete([len(pool) for pool in self.action_pools.values()])
+        
+        self.observation_space = spaces.Box(low=-300, high=300, shape=(6,), dtype=np.float32)
+        self.game.set_available_actions([action_name for action_name in self.action_pools.keys()])
+        self.game.init()
+        self.game.new_episode()
+        if config["use_depth"]:
+            self.game.turn_on_depth_map()
+            width , height , max_depth = self.game.get_depth_map_size()
+            self.observation_space = spaces.Tuple([spaces.Box(low=-300, high=300, shape=(6,), dtype=np.float32),spaces.Box(0, max_depth, (height, width), dtype=np.float32)])
+
+        location = self.game.get_valid_locations()
+        indoor_loc = location["indoor"]
+        outdoor_loc = location["outdoor"]
+        self.valid_loc_1000 = []
+        self.valid_loc_3000 = []
+        self.valid_loc_5000 = []
+        for loc in indoor_loc:
+            dis = get_distance(loc, self.target_location)
+            if dis <= 50:
+                self.valid_loc_1000.append(loc)
+            elif dis <= 100:
+                self.valid_loc_3000.append(loc)
+            elif dis <= 200:
+                self.valid_loc_5000.append(loc)
+
+
 
     def _reset_game_config(self):
         self.start_location = self._sample_start_location()
@@ -75,16 +139,16 @@ class NavigationBaseEnv(BaseEnv):
         self.game.make_action({0: action_cmd})
         self.state = self.game.get_state()
         done = self.game.is_episode_finished()
-        reward = 0
+        
         self.running_steps += 1
-
+        cur_pos = get_position(self.state)
+        tar_pos = self.target_location
+        # reward = -get_distance(cur_pos, tar_pos)
+        reward =0
+        if get_distance(cur_pos, tar_pos) <= 1:
+            reward += 100
+            done = True
         if done:
-            cur_pos = get_position(self.state)
-            tar_pos = self.target_location
-
-            if get_distance(cur_pos, tar_pos) <= self.trigger_range:
-                reward += 100
-            
             if self.print_log:
                 Start = np.round(np.asarray(self.start_location), 2).tolist()
                 Target = np.round(np.asarray(self.target_location), 2).tolist()
@@ -95,42 +159,37 @@ class NavigationBaseEnv(BaseEnv):
 
         return self._get_obs(), reward, done, {}
 
-    def _sample_start_location(self):
-        raise NotImplementedError()
-
-    def _action_process(self, action):
-        raise NotImplementedError()
-
-
-class NavigationEnvSimple(NavigationBaseEnv):
-    def __init__(self, config: EnvContext):
-        super().__init__(config)
-        self.action_pools = {
-            ActionVariable.WALK_DIR: [0, 90, 180, 270],
-            ActionVariable.WALK_SPEED: [3, 6],
-        }
-        self.action_space = spaces.MultiDiscrete([len(pool) for pool in self.action_pools.values()])
-        self.observation_space = spaces.Box(low=-1, high=1, shape=(3,), dtype=np.float32)
-
-        self.game.set_available_actions([action_name for action_name in self.action_pools.keys()])
-        self.game.init()
 
     def _get_obs(self):
         cur_pos = np.asarray(get_position(self.state))
         tar_pos = np.asarray(self.target_location)
         dir_vec = tar_pos - cur_pos
-        return dir_vec / np.linalg.norm(dir_vec)
+        obs = np.concatenate([cur_pos,tar_pos])
+
+        if self.config["use_depth"]:
+            obs = np.concatenate([obs,self.state.depth_map],axis=1)
+        return obs
 
     def _action_process(self, action):
         action_values = list(self.action_pools.values())
         return [action_values[i][action[i]] for i in range(len(action))]
 
     def _sample_start_location(self):
-        angle = np.random.uniform(0, 360)
-        distance_to_trigger = abs(np.random.normal(scale=self.start_range))
-        vec_len = self.trigger_range + distance_to_trigger
-        dx = np.sin(angle) * vec_len
-        dz = np.cos(angle) * vec_len
-        x = self.target_location[0] + dx
-        z = self.target_location[2] + dz
-        return [x, self.start_hight, z]
+        # angle = np.random.uniform(0, 360)
+        # distance_to_trigger = abs(np.random.normal(scale=self.start_range))
+        # vec_len = self.trigger_range + distance_to_trigger
+        # dx = np.sin(angle) * vec_len
+        # dz = np.cos(angle) * vec_len
+        # x = self.target_location[0] + np.random.randint(-30,30)
+        # z = self.target_location[2] + np.random.randint(-30,30)
+
+        if self.episodes<=1000:
+            loc = random.choice(self.valid_loc_1000)
+        elif self.episodes<=3000:
+            loc = random.choice(self.valid_loc_3000)
+        else:
+            loc = random.choice(self.valid_loc_5000)
+
+        return loc
+
+
